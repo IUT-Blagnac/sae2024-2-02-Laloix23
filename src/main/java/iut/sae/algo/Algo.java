8eebde17e60b2b07fd92b5a5db5d658a9d20699a
IUT BLAGNAC <noreply@univ-tlse2.fr>
2024-06-06 12:25:01 +0200

    MAJ

--- src/main/java/iut/sae/algo/Algo.java
+++ src/main/java/iut/sae/algo/Algo.java
@@ -3,8 +3,11 @@ package iut.sae.algo;
 
 public class Algo{
     public static String RLE(String in){
-        // Provide your algo here
-        return "NotYetImplemented";
+        String newChaine= "";
+        for(int i=0;i<in.length(); i++){
+            System.out.println(in.charAt(i));
+        }
+        return newChaine;
     }
 
     public static String RLE(String in, int iteration) throws AlgoException{
